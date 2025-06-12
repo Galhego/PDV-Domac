d721d16f90d57061e1a80ddddf82a1d609063187
BCSAL <124710121+BryanCSAL@users.noreply.github.com>
2025-06-12 19:06:01 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbname = "pdv_php";
 $user = "root";
-$password = "";
+$password = "1234@Eurismar";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
